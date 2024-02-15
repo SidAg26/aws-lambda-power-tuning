38304f84ecf09d9ae77f6f08c77fc92bdfcc1a9e
SidAg26 <siddharth13103410@gmail.com>
2024-02-15 02:58:28 +0000

    push fix-input-dict-error

--- similarity-lambda-workflow/offline-step/preProcessInputList.py
+++ similarity-lambda-workflow/offline-step/preProcessInputList.py
@@ -1,10 +1,14 @@
 def lambda_handler(event, context):
     input_dict = event['payload']
-    i_min = input_dict['min'] if 'min' in input_dict else 0
-    i_max = input_dict['max'] if 'max' in input_dict else 100
-    i_step = input_dict['stepSize'] if 'stepSize' in input_dict else None
-
-    if not i_step:
+    if isinstance(input_dict, dict):
+        i_min = input_dict['min'] if 'min' in input_dict else 0
+        i_max = input_dict['max'] if 'max' in input_dict else 100
+        i_step = input_dict['stepSize'] if 'stepSize' in input_dict else None
+    else:
+        i_min = 0
+        i_max = 100
+        i_step = None
+    if i_step:
         input_list = [i for i in range(i_min, i_max, i_step)]
     else:
         input_list = [i_min, i_max]
