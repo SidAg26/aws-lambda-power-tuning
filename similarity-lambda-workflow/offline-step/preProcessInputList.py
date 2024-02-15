908734d74fcfe30884bf924cca0c8c2ba6840379
SidAg26 <siddharth13103410@gmail.com>
2024-02-15 02:30:26 +0000

    push remove-checkStepSize-state-update-preProcessInputList

--- similarity-lambda-workflow/offline-step/preProcessInputList.py
+++ similarity-lambda-workflow/offline-step/preProcessInputList.py
@@ -1,8 +1,12 @@
 def lambda_handler(event, context):
     input_dict = event['payload']
-    i_min = input_dict['min']
-    i_max = input_dict['max']
-    i_step = input_dict['stepSize']
-    
-    input_list = [i for i in range(i_min, i_max, i_step)]
+    i_min = input_dict['min'] if 'min' in input_dict else 0
+    i_max = input_dict['max'] if 'max' in input_dict else 100
+    i_step = input_dict['stepSize'] if 'stepSize' in input_dict else None
+
+    if not i_step:
+        input_list = [i for i in range(i_min, i_max, i_step)]
+    else:
+        input_list = [i_min, i_max]
+
     return input_list
